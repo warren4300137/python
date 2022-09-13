75f17d184b57b0054da3244286b1d62a6facd877
Warren Wu <warren4300137@gmail.com>
2022-09-13 16:19:58 +0800

    1. add more unitest for red_black_tree insert

--- data_structure/red_black_tree/test_red_black_tree.py
+++ data_structure/red_black_tree/test_red_black_tree.py
@@ -9,7 +9,84 @@ class TestRedBlackTree(unittest.TestCase):
     def tearDown(self):
         self.rb = None
 
-    def test_insert(self):
+    def test_insert_empty(self):
+        expect_in_order = []
+        expect_level_order = []
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
+    
+    def test_insert_case_1(self):
+        expect_in_order = [1]
+        expect_level_order = [1]
+
+        self.rb.insert(1)
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
+        self.assertTrue(self.rb.root.color == 'B')
+
+    def test_insert_case_2(self):
+        expect_in_order = [1, 2]
+        expect_level_order = [1, 2]
+
+        self.rb.insert(1)
+        self.rb.insert(2)
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
+        self.assertTrue(self.rb.root.color == 'B')
+        self.assertTrue(self.rb.root.right.color == 'R')
+
+    def test_insert_case_3(self):
+        expect_in_order = [1, 2, 3]
+        expect_level_order = [2, 1, 3]
+
+        self.rb.insert(1)
+        self.rb.insert(2)
+        self.rb.insert(3)
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
+        self.assertTrue(self.rb.root.color == 'B')
+        self.assertTrue(self.rb.root.left.color == 'R')
+        self.assertTrue(self.rb.root.right.color == 'R')
+
+    def test_insert_case_4(self):
+        expect_in_order = [1, 2, 3, 4]
+        expect_level_order = [2, 1, 3, 4]
+
+        self.rb.insert(1)
+        self.rb.insert(2)
+        self.rb.insert(3)
+        self.rb.insert(4)
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
+        self.assertTrue(self.rb.root.color == 'B')
+        self.assertTrue(self.rb.root.left.color == 'B')
+        self.assertTrue(self.rb.root.right.color == 'B')
+        self.assertTrue(self.rb.root.right.right.color == 'R')
+
+    def test_insert_case_5(self):
+        expect_in_order = [1, 2, 3, 4, 5]
+        expect_level_order = [2, 1, 4, 3, 5]
+
+        self.rb.insert(1)
+        self.rb.insert(2)
+        self.rb.insert(3)
+        self.rb.insert(4)
+        self.rb.insert(5)
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
+        self.assertTrue(self.rb.root.color == 'B')
+        self.assertTrue(self.rb.root.left.color == 'B')
+        self.assertTrue(self.rb.root.right.color == 'B')
+        self.assertTrue(self.rb.root.right.left.color == 'R')
+        self.assertTrue(self.rb.root.right.right.color == 'R')
+
+    def test_insert_case_large_1(self):
         expect_in_order = [1, 2, 3, 4, 5, 6, 7]
         expect_level_order = [6, 4, 7, 2, 5, 1, 3]
         self.rb.insert(7)
@@ -23,6 +100,20 @@ class TestRedBlackTree(unittest.TestCase):
         self.assertEqual(expect_in_order, self.rb.inorder())
         self.assertEqual(expect_level_order, self.rb.levelOrder())
 
+    def test_insert_case_large_2(self):
+        expect_in_order = [1, 2, 3, 4, 5, 6, 7]
+        expect_level_order = [2, 1, 4, 3, 6, 5, 7]
+
+        self.rb.insert(1)
+        self.rb.insert(2)
+        self.rb.insert(3)
+        self.rb.insert(4)
+        self.rb.insert(5)
+        self.rb.insert(6)
+        self.rb.insert(7)
+
+        self.assertEqual(expect_in_order, self.rb.inorder())
+        self.assertEqual(expect_level_order, self.rb.levelOrder())
 
 if __name__=='__main__':
     unittest.main()
